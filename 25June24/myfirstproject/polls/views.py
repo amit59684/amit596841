fdea327ae28a89dbc9b52224d50a95059794be4d
amit59684 <107456534+amit59684@users.noreply.github.com>
2024-06-26 10:50:15 +0530

    files

---
+++ 25June24/myfirstproject/polls/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import  get_object_or_404,render
+from django.http import HttpResponse
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
